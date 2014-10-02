90e9d797abc24476feb6fb0c1c51c480bf494921
Martijn van Maurik <martijn@vmaurik.nl>
2014-10-02 11:13:26 +0200

    Add custom php ini file

--- start.sh
+++ start.sh
@@ -4,6 +4,7 @@ DATADIR="/data"
 [[ -d "/data/" ]] || mkdir /data
 [[ -d "/data/config" ]] || mkdir /data/config
 [[ -e "/data/config/nginx-default.conf" ]] || cp /etc/nginx/sites-available/default /data/config/nginx-default.conf
+([[ -e "/data/config/php-user.ini" ]] && [[ -e "/etc/php5/fpm/conf.d/999-user.ini" ]]) || ln -s /data/config/php-user.ini /etc/php5/fpm/conf.d/999-user.ini
 [[ -d "/data/web" ]] || mkdir /data/web
 chown www-data: /data -R
 
